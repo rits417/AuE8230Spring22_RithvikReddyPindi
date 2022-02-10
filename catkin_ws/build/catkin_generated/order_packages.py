91c7a02ce4111ad00f7673c24a25d8b98e7de459
rits417 <rpindi@clemson.edu>
2022-02-09 23:50:20 -0500

    first commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rithvikreddy/AuE8230Spring22_RithvikReddyPindi/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rithvikreddy/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/rithvikreddy/catkin_ws/devel;/opt/ros/noetic' != '' else []
