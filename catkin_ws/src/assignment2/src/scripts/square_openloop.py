3fe6bead58951510308137e24f7e044ea0ceac86
rits417 <rpindi@clemson.edu>
2022-02-10 11:43:21 -0500

    new

--- catkin_ws/src/assignment2/src/scripts/square_openloop.py
+++ catkin_ws/src/assignment2/src/scripts/square_openloop.py
@@ -8,18 +8,15 @@ def move():
     rospy.init_node('robot_cleaner', anonymous=True)
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     vel_msg = Twist()
-
-    print('Lets make the turtlesim make a square')
+    print('creating a square using turtlesim')
     x = 1
     while(x<10):
-
         vel_msg.angular.x = 0
         vel_msg.angular.y = 0
         vel_msg.angular.z = 0
         vel_msg.linear.x = 0.2
         vel_msg.linear.y = 0
         vel_msg.linear.z = 0
-
         t0 = rospy.Time.now().to_sec()
         distance = 0
 
@@ -38,7 +35,6 @@ def move():
             angle= 0.2*(tb-ta)
         vel_msg.angular.z = 0
         x = x+1
-        #Making the robot stop
     velocity_publisher.publish(vel_msg)
 
 if __name__ == '__main__':
